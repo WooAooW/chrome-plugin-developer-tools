f1f97c25dbd276e6517ba8b151bb0f507086ba12
helee <kissaoe@gmail.com>
2024-01-13 20:49:40 +0800

    :sparkles: 新增[新页面打开]功能

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 
 ## 依赖库
 
+* [Vue3](https://vuejs.org/)
+* [Naice UI](https://www.naiveui.com/zh-CN/light)
+* [Vite](https://vitejs.dev/)
+* [VueUse](https://vueuse.org/)
 
 ## 本地开发
 开发环境:
@@ -10,7 +14,8 @@
 * [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur)
 * [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
 
-执行命令:
+1. 执行命令: `pnpm run dev` 运行。
+2. 浏览器打开 `http://localhost:5173/` 
 
 ## 使用插件
 
--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
 <template>
+    <div v-if="isSidePanel">
+        <n-button type="info" @click="openNewTab">新页面使用</n-button>
+        <n-p/>
+    </div>
     <n-tabs type="card">
         <n-tab-pane key="rule" name="个人收藏" display-directive="show">
             <Favorites/>
@@ -13,8 +17,46 @@
 </template>
   
 <script setup lang="ts">
-import { NTabs, NTabPane } from 'naive-ui'
+import { ref } from 'vue'
+import { NTabs, NTabPane, NButton, NP, createDiscreteApi } from 'naive-ui'
 import Favorites from './components/Favorites.vue'
 import TimeUtils from './components/time/TimeUtils.vue'
 import M3U8Utils from './components/m3u8/M3U8Utils.vue'
+
+// 消息提示框
+const { message } = createDiscreteApi(['message', 'dialog', 'notification', 'loadingBar'])
+
+// 判断是否在侧边栏
+const isSidePanel = ref(true)
+
+// 打开新的标签页
+const openNewTab = () => {
+    window.open('index.html')
+}
+
+// 加载页面信息
+const loadTabInfo = () => {
+    const queryInfo = {
+        active: true,
+        currentWindow: true
+    }
+    if(!chrome) {
+        message.warning('当前环境不支持 chrome 插件!')
+        return;
+    }
+    if(!chrome.tabs) {
+        message.warning('插件无法申请到 tabs 权限, 请确定当前活动标签为 wiki 页面!')
+        return;
+    }
+    chrome.tabs.query(queryInfo, tabs => {
+        const url = tabs[0].url
+        if(url?.startsWith("chrome-extension://")) {
+            isSidePanel.value = false
+        } else {
+            isSidePanel.value = true
+        }
+    })
+    return true
+}
+loadTabInfo()
 </script>
